06099cdfe55a096db14969831d6637d8f74a81b8
Chris Cox cox.crc@gmail.com <christopher.cox-2@manchester.ac.uk>
2018-03-26 19:38:35 +0100

    progress

--- bin/UnwravelResults.m
+++ bin/UnwravelResults.m
@@ -1,11 +1,16 @@
-function [ output_args ] = UnwravelResults( ResultsDir )
+function UnitCodes = UnwravelResults( ResultsDir )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
     results = LoadSimulationResults( ResultsDir );
-    load('/Users/Chris/src/SOSLassoSimulations/data/AnnotatedData.mat');
-    load('/Users/Chris/src/SOSLassoSimulations/data/ConditionIndex.mat');
-    z = ConditionIndex.condition == results(1).condition & ...
-        ConditionIndex.subject == num2str(results(1).subject);
-    
+    load('C:/Users/mbmhscc4/GitHub/SOSLassoSimulations/data/AnnotatedData.mat');
+    load('C:/Users/mbmhscc4/GitHub/SOSLassoSimulations/data/ConditionIndex.mat');
+    UCW = cell(numel(results), 1);
+    for i = 1:numel(results);
+        SortInfo = select_sort_info(ConditionIndex, results(i));
+        UnitCodes = select_unit_codes(AnnotatedData, SortInfo, results(i));
+        UnitCodes.weights = results(1).Wz;
+        UCW{i} = UnitCodes;
+    end
+    UnitCodes = cat(1,UCW{:});
 end
-
+    
