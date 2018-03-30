0eabacabe18d5b35ad13bf5755a6c80f46a808cb
Chris Cox cox.crc@gmail.com <christopher.cox-2@manchester.ac.uk>
2018-03-30 19:29:15 +0100

    Select specific subject data and drop example fields

--- bin/select_unit_codes.m
+++ bin/select_unit_codes.m
@@ -1,14 +1,22 @@
 function UnitCodes = select_unit_codes(d, varargin)
     p = inputParser();
     addRequired(p, 'd', @istable);
+    addParameter(p, 'subject', '1');
     addParameter(p, 'DropPadding', false, @islogical);
     parse(p, d, varargin{:});
     
-    z = d.subject == '1' & ...
+    if isnumeric(p.Results.subject)
+        s = num2str(p.Results.subject);
+    else
+        s = p.Results.subject;
+    end
+    z = d.subject == s & ...
         d.example_id == '1';
     if p.Results.DropPadding
     	z = z & d.unit_category ~= 'padding';
     end
+    d.example_category = [];
+    d.example_id = [];
     UnitCodes = sortrows(d(z,:),'padded_unit_id');
 %     UnitCodes = apply_sort_to_unit_codes(UnitCodes,SortInfo);
 end
