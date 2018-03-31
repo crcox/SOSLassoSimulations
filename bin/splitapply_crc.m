7a921efe0665d084954fb08c8ab904e5233c2993
Chris Cox <crcox@wisc.edu>
2018-03-31 20:26:07 +0100

    Removed cell2mat, now unpack single cells.

--- bin/splitapply_crc.m
+++ bin/splitapply_crc.m
@@ -1,4 +1,12 @@
 function x = splitapply_crc(func, tab, grp)
     grp_id = unique(grp);
-    x = cell2mat(arrayfun(@(g) func(tab(grp==g,:)), grp_id, 'UniformOutput', 0));
+    X = arrayfun(@(g) func(tab(grp==g,:)), grp_id, 'UniformOutput', false);
+    x = cellfun(@unpackSingleCell, X, 'UniformOutput', false);
+%     x = cell2mat(X);
+end
+
+function x = unpackSingleCell(x)
+    if iscell(x) && numel(x) == 1
+        x = x{1};
+    end
 end
