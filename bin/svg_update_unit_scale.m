6fc0b06aa8123a1e94a3d5f311c591e3fd3fb80f
Chris Cox cox.crc@gmail.com <christopher.cox-2@manchester.ac.uk>
2018-04-03 20:32:32 +0100

    Adjusted to help handle the unit scaling bug

--- bin/svg_update_unit_scale.m
+++ bin/svg_update_unit_scale.m
@@ -1,6 +1,16 @@
-function [ output_args ] = svg_update_unit_scale( svgDOM, unitID, scale )
+function [rx,ry] = svg_update_unit_scale( svgDOM, unitID, scale, method )
+    if nargin < 4
+        method = 'proportional';
+    end
     unit_list = svgDOM.getElementsByTagName('ellipse');
     unit = svg_selectunit(unit_list, unitID);
     [rx,ry] = svg_getunitradii(unit);
-    svg_setunitradii(unit, [rx,ry] .* scale);
+    switch method
+        case 'proportional'
+            if scale ~= 1
+                svg_setunitradii(unit, [rx,ry] .* scale);
+            end
+        case 'absolute'
+            svg_setunitradii(unit, scale);
+    end
 end
