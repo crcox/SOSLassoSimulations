364592ca203a54dab17639a90b5826a4df198562
Chris Cox cox.crc@gmail.com <christopher.cox-2@manchester.ac.uk>
2018-04-03 20:32:08 +0100

    moved out most of the code into a new function, apply statistical analysis

--- bin/MakeNetworkFigure.m
+++ bin/MakeNetworkFigure.m
@@ -1,51 +1,49 @@
-function [ ] = MakeNetworkFigure( UnitCodes, varargin)
+function [UnitCodesByCond] = MakeNetworkFigure( UnitCodesByCond, varargin)
     p = inputParser();
     addRequired(p, 'UnitCodes', @istable);
     addOptional(p, 'BaseProbability', table(), @istable);
     addParameter(p, 'Template', '../../../../template/ModelSchematic_ColorCoded.svg',@ischar);
-    addParameter(p, 'PThreshold', 0.01, @isscalar);
-    parse(p, UnitCodes, varargin{:});
+    addParameter(p, 'PThreshold', 0.05, @isscalar);
+    addParameter(p, 'FDRCorrect', true, @islogical);
+    addParameter(p, 'AnalysisType', 'unspecified', @(x) any(strcmp(x,{'ridge','lasso','soslasso','univariate','searchlight'})));
+    parse(p, UnitCodesByCond, varargin{:});
     
-    splitapply_local = defineIfNotBuiltin('splitapply', @splitapply_crc);
-    UnitCodes = merge_baseprobability(p.Results.UnitCodes,p.Results.BaseProbability);
-    isSearchlight = any(strcmp('radius',UnitCodes.Properties.VariableNames));
-    isUnivariate = any(strcmp('lme',UnitCodes.Properties.VariableNames));
-    
-    svgDOM = xmlread(p.Results.Template);
-    if isSearchlight
-        variablesOfInterest = {'unit_category','unit_id_by_category','unit_contribution','padded_unit_id','condition','radius'};
-        [UnitCodesByCond,~,G] = unique(UnitCodes(:,variablesOfInterest));
-        ttest_output = splitapply_local(@myttest, UnitCodes.accuracy - 0.5, G);
-        [~,~,~,UnitCodesByCond.pval] = fdr_bh(ttest_output(:,1),p.Results.PThreshold);
-        UnitCodesByCond.tstat = ttest_output(:,2);
-        UnitCodesByCond.rgb = generate_rgb_searchlight(UnitCodesByCond);
-    elseif isUnivariate
-        UnitCodesByCond = UnitCodes;
-        [~,~,~,UnitCodesByCond.pval] = fdr_bh(UnitCodes.pval);
-        UnitCodesByCond.rgb = generate_rgb_univariate(UnitCodesByCond);        
-    else
-        nsubj = numel(categories(UnitCodes.subject));
-        variablesOfInterest = {'unit_category','unit_id_by_category','unit_contribution','padded_unit_id','condition'};
-        [UnitCodesByCond,~,G] = unique(UnitCodes(:,variablesOfInterest));
-        UnitCodesByCond.baseprob = splitapply_local(@mean, UnitCodes.baseprob, G);
-        UnitCodesByCond.n_nz = splitapply_local(@nnz, UnitCodes.weights ~= 0, G);
-        UnitCodesByCond.n_pos = splitapply_local(@nnz, UnitCodes.weights > 0, G);
-        UnitCodesByCond.n_neg = splitapply_local(@nnz, UnitCodes.weights < 0, G);
-        UnitCodesByCond.pval = binopdf(UnitCodesByCond.n_nz, nsubj, UnitCodesByCond.baseprob);
-        UnitCodesByCond.rgb = generate_rgb_lasso(UnitCodesByCond);
-    end
-    
-    UnitCodesByCond.hex = generate_hex(UnitCodesByCond);
-    UnitCodesByCond.hex_sig = generate_hex(UnitCodesByCond, p.Results.PThreshold);
-    
-    if isSearchlight
-        generate_figures_searchlight(svgDOM, UnitCodesByCond)
-    elseif isUnivariate
-        generate_figures_univariate(svgDOM, UnitCodesByCond)
-    else
-        generate_figures_wholebrain_mvpa(svgDOM, UnitCodesByCond, nsubj)
+    if p.Results.FDRCorrect
+        [~,crit_p,~,UnitCodesByCond.pval] = fdr_bh(UnitCodesByCond.pval,p.Results.PThreshold);
     end
 
+    nsubj = 10;
+    svgDOM = xmlread(p.Results.Template);
+    switch p.Results.AnalysisType
+        case 'ridge'
+            UnitCodesByCond.rgb = generate_rgb_lasso(UnitCodesByCond);
+            UnitCodesByCond.hex = generate_hex(UnitCodesByCond);
+            UnitCodesByCond.hex_sig = generate_hex(UnitCodesByCond, p.Results.PThreshold);
+            generate_figures_wholebrain_mvpa(svgDOM, UnitCodesByCond, nsubj)
+
+        case 'univariate'
+            UnitCodesByCond.rgb = generate_rgb_univariate(UnitCodesByCond);
+            UnitCodesByCond.hex = generate_hex(UnitCodesByCond);
+            UnitCodesByCond.hex_sig = generate_hex(UnitCodesByCond, p.Results.PThreshold);
+            generate_figures_univariate(svgDOM, UnitCodesByCond)
+            
+        case 'searchlight'
+            crit_t = tinv(crit_p, 9);
+            UnitCodesByCond.rgb = generate_rgb_searchlight(UnitCodesByCond,crit_t);
+            UnitCodesByCond.hex = generate_hex(UnitCodesByCond);
+            UnitCodesByCond.hex_sig = generate_hex(UnitCodesByCond, p.Results.PThreshold);
+            generate_figures_searchlight(svgDOM, UnitCodesByCond)
+            
+        case {'lasso','soslasso'}
+            UnitCodesByCond.rgb = generate_rgb_lasso(UnitCodesByCond);
+            UnitCodesByCond.hex = generate_hex(UnitCodesByCond);
+            UnitCodesByCond.hex_sig = generate_hex(UnitCodesByCond, p.Results.PThreshold);
+            generate_figures_wholebrain_mvpa(svgDOM, UnitCodesByCond, nsubj)
+            
+        case 'unspecified'
+            error('An analysis type must be specified for the data to be properly prepared.')
+
+    end
 end
 
 function UnitCode = getUnitCode(uc)
@@ -53,8 +51,9 @@ function UnitCode = getUnitCode(uc)
 end
 
 function rgb = generate_rgb_univariate(UnitCodesByCond)
-    r = UnitCodesByCond.tstat > 0 + 0;
-    b = UnitCodesByCond.tstat <= 0 + 0;
+    % Positive effects are voting for B
+    r = UnitCodesByCond.tstat <= 0 + 0;
+    b = UnitCodesByCond.tstat > 0 + 0;
     g = zeros(numel(b),1);
     rgb = [r,g,b];
 end
@@ -71,11 +70,12 @@ function rgb = generate_rgb_lasso(UnitCodesByCond)
     rgb = [r,g,b];
 end
 
-function rgb = generate_rgb_searchlight(UnitCodesByCond)
+function rgb = generate_rgb_searchlight(UnitCodesByCond, minT)
     % No red
     % No blue
     % Green intensifies as tstat increases
-    g = min(abs(UnitCodesByCond.tstat)/9, 1);
+    x = max(UnitCodesByCond.tstat-minT,0); % set T <= minT to zero
+    g = min(x/(9-minT), 1);
     r = zeros(numel(g),1);
     b = zeros(numel(g),1);
     rgb = [r,g,b];
@@ -108,15 +108,28 @@ end
 
 function [] = generate_figures_wholebrain_mvpa(svgDOM, UnitCodesByCond, nsubj)
     conditions = categories(UnitCodesByCond.condition);
+    OriginalScales = svg_get_original_unit_scales(svgDOM, UnitCodesByCond);
     for j = 1:numel(conditions)
         z = UnitCodesByCond.condition == conditions{j};
         uc = UnitCodesByCond(z,:);
         for i = 1:size(uc, 1)
             UnitCode = getUnitCode(uc(i,:));
             svg_update_unit_fill(svgDOM,UnitCode,uc.hex_sig(i));
-            svg_update_unit_scale(svgDOM,UnitCode,uc.n_nz(i)/nsubj);
+            svg_update_unit_scale(svgDOM,UnitCode,OriginalScales(i,:),'absolute');
+            svg_update_unit_scale(svgDOM,UnitCode,uc.n_nz(i)/nsubj,'proportional');
         end
         xmlwrite(sprintf('%s.svg',conditions{j}), svgDOM);
+    end
+end
+
+function OriginalScales = svg_get_original_unit_scales(svgDOM, UnitCodesByCond)
+    z = UnitCodesByCond.condition == UnitCodesByCond.condition(1);
+    uc = UnitCodesByCond(z,:);
+    OriginalScales = zeros(nnz(z),2);
+    for i = 1:size(uc, 1)
+        UnitCode = getUnitCode(uc(i,:));
+        [rx,ry] = svg_update_unit_scale(svgDOM,UnitCode,1);
+        OriginalScales(i,:) = [rx,ry];
     end
 end
 
@@ -135,18 +148,4 @@ function [] = generate_figures_searchlight(svgDOM, UnitCodesByCond)
             xmlwrite(sprintf('%s_radius%d.svg',conditions{j},radii(k)), svgDOM);
         end
     end
-end
-
-function m = merge_baseprobability(uc,bp)
-    if isempty(bp)
-        m = uc;
-    else
-        m = join(uc,bp);
-    end
-end
-
-function y = myttest(x,varargin)
-    [~,pval,~,stats] = ttest(x,varargin{:});
-    tstat = stats.tstat;
-    y = [pval,tstat];
 end
